fe1c06dc1585c4b5f043a87e07ac32671f3908aa
Anthony Green <green@moxielogic.com>
2024-06-12 08:02:26 -0400

    Fix

--- run.sh
+++ run.sh
@@ -13,16 +13,16 @@ trap cleanup EXIT
 
 function trivy_scan {
     # Perform trivy scans
-    IMG=$(echo ${2} | sed 's/regis.*\///g')
-    mkdir -p ${1}/trivy
-    trivy -f json -o ${1}/trivy/${IMG}.json image ${2}:latest
+    IMG=$(echo ${2} | sed 's/\//\-\-/g')
+    mkdir -p ${IMG}/trivy
+    trivy -f json -o ${IMG}/trivy/${IMG}.json image ${2}:latest
 }
 
 function grype_scan {
     # Perform grype scans
-    IMG=$(echo ${2} | sed 's/regis.*\///g')
-    mkdir -p ${1}/grype
-    grype -o json=${1}/grype/${IMG}.json ${2}:latest
+    IMG=$(echo ${2} | sed 's/\//\-\-/g')
+    mkdir -p ${IMG}/grype
+    grype -o json=${IMG}/grype/${IMG}.json ${2}:latest
 }
 
 for IMAGE in registry.access.redhat.com/ubi9 registry.access.redhat.com/ubi8 registry.access.redhat.com/ubi9-minimal registry.access.redhat.com/ubi8-minimal amazonlinux fedora; do
@@ -39,7 +39,8 @@ EOF
     trivy_scan ${SCANDIR} ${IMAGE}-with-updates
     grype_scan ${SCANDIR} ${IMAGE}-with-updates
 
-    (cd ${WORKDIR}; tar cvfz ${IMAGE}-scanalisys.tar.gz ${IMAGE})
+    IMG=$(echo ${IMAGE} | sed 's/regis.*\///g')
+    (cd ${WORKDIR}; tar cvfz ${IMG}-scanalisys.tar.gz ${IMG})
 done
 
 cp ${WORKDIR}/*.tar.gz .
