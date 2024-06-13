ca1fa2418db550ae2b84539554fd8d27e3d7a974
Anthony Green <green@moxielogic.com>
2024-06-13 19:53:26 -0400

    Fix

--- report.lisp
+++ report.lisp
@@ -305,9 +305,9 @@ code {
 
 (defun describe-container (image)
   (cond
-    ((search "ubi8 image")
+    ((search "ubi8" "image")
      "RHEL 8 UBI")
-    ((search "ubi9" image)
+    ((search "ubi9" "image")
      "RHEL 9 UBI")
     (t
      image)))
