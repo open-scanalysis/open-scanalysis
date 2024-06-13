30b97a3cf53f4ac3559a0b942458ef046832ab10
Anthony Green <green@moxielogic.com>
2024-06-13 07:05:26 -0400

    Generate reports

--- run.sh
+++ run.sh
@@ -25,6 +25,11 @@ function grype_scan {
     grype -o json=${1}/grype/${IMG}.json ${2}:latest
 }
 
+apt update
+apt install sbcl
+git clone https://github.com/ocicl/ocicl
+(cd ocicl; sbcl --load setup.lisp; ocicl setup > ~/.sbclrc)
+
 for IMAGE in registry.access.redhat.com/ubi9 registry.access.redhat.com/ubi8 registry.access.redhat.com/ubi9-minimal registry.access.redhat.com/ubi8-minimal amazonlinux fedora; do
 
     SCANDIR=${WORKDIR}/$(echo ${IMAGE} | sed -e 's/regi.*\///g')
@@ -42,10 +47,14 @@ EOF
 
     IMG=$(echo ${IMAGE}-with-updates | sed 's/\//\-\-/g')
     VERSION=$(date +%Y%m%d)
+
+    sbcl --load report.lisp $(pwd)/public/${IMG}.html ${SCANDIR}/grype/* ${SCANDIR}/trivy/*
+
     (cd ${WORKDIR};
      tar cvfz ${IMG}-scanalysis.tar.gz * ;
      oras push ghcr.io/open-scanalysis/${IMG}:${VERSION} ${IMG}-scanalysis.tar.gz:application/vnd.uknown/layer.v1+gzip ;
      oras tag ghcr.io/open-scanalysis/${IMG}:${VERSION} latest
+
      rm -rf *
     )
 done
